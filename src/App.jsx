963f13f459b01bb071c40f07cd3b61b5d4655369
faheem-1789 <faheemiqbal993@gmail.com>
2025-09-20 00:50:42 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ import { motion, AnimatePresence } from 'framer-motion';
 
 // --- Firebase Configuration ---
 const firebaseConfig = {
-    apiKey: "AIzaSyAA6U-oPKefpOdyIsS6wXVmjgCTj3Jlow",
+    apiKey: "AIzaSyAA6U-oPKefpOdy6IsS6wXVmjgCTj3Jlow",
     authDomain: "sop-assistant-9dc2a.firebaseapp.com",
     projectId: "sop-assistant-9dc2a",
     storageBucket: "sop-assistant-9dc2a.appspot.com",
